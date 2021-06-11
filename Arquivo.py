03c834d901f4e49eca522131c063b9b1b194da73
Pedro Costa <pedroc_aragao@outlook.com>
2021-06-10 23:36:52 -0300

    Adicionada pergunta se usuário quer salvar antes de sair ou criar novo arquivo caso feitas alterações

--- Arquivo.py
+++ Arquivo.py
@@ -1,4 +1,4 @@
-from tkinter import filedialog as fd
+from tkinter import filedialog as fd, messagebox as mb
 from tkinter import *
 
 class Arquivo:
@@ -10,9 +10,10 @@ class Arquivo:
             ('HTML', '*.html')
         )
     
+
     def __init__(self):
         self.__arquivo_atual = ''
-
+        self.__conteudo_arquivo = ''
 
     @property
     def arquivo_atual(self):
@@ -24,18 +25,49 @@ class Arquivo:
         self.__arquivo_atual = caminho
 
 
-    def novo_arquivo(self):
-        pass
+    @property
+    def conteudo_arquivo(self):
+        return self.__conteudo_arquivo
+    
+
+    @conteudo_arquivo.setter
+    def conteudo_arquivo(self, conteudo):
+        self.__conteudo_arquivo = conteudo
+    
+
+    def arquivo_modificado(self, text_space):
+        return self.conteudo_arquivo != text_space.get('1.0', END)[:-1]
+
+
+    def perguntar_salvar(self, text_space):
+        confirma = mb.askyesno(
+            title='Bloco de Notas', 
+            message='Você deseja salvar as alterações realizadas?'
+        )
+        if confirma:
+            self.salvar_arquivo(text_space)
+        elif confirma == None:
+            return
+
+
+    def novo_arquivo(self, text_space):
+        if self.arquivo_modificado(text_space):
+            self.perguntar_salvar(text_space)
+        self.conteudo_arquivo = ''
+        self.arquivo_atual = ''
+        text_space.delete('1.0', END)      
 
     
     def abrir_arquivo(self, text_space):
+        if self.arquivo_modificado(text_space):
+            self.perguntar_salvar(text_space)
         caminho_arquivo = fd.askopenfilename(filetypes=Arquivo.filetypes)
         if caminho_arquivo:
             self.arquivo_atual = caminho_arquivo
             with open(self.arquivo_atual, 'r') as arquivo:
-                texto_arquivo = arquivo.read()
+                self.conteudo_arquivo = arquivo.read()[:-1]
                 text_space.delete('1.0', END)
-                text_space.insert('1.0', texto_arquivo)
+                text_space.insert('1.0', self.conteudo_arquivo)
 
     
     def salvar_arquivo(self, text_space):
@@ -44,6 +76,7 @@ class Arquivo:
                 arquivo.write(text_space.get('1.0', END))
         else:
             self.salvar_como(text_space)
+        self.conteudo_arquivo = text_space.get('1.0', END)[:-1]
     
     
     def salvar_como(self, text_space):
@@ -54,3 +87,16 @@ class Arquivo:
         if caminho_arquivo:
             self.arquivo_atual = caminho_arquivo
             self.salvar_arquivo(text_space)
+
+    
+    def fechar_bloco(self, text_space, root):
+        if self.arquivo_modificado(text_space):
+            resp = mb.askyesnocancel(
+                title='Bloco de Notas',
+                message='Deseja salvar as alterações antes de fechar o Bloco de Notas?'
+            )
+            if resp == None:
+                return
+            elif resp:
+                self.salvar_arquivo(text_space, root)
+        root.quit()
--- main.py
+++ main.py
@@ -12,12 +12,12 @@ barra_menus = Menu(root)
 
 menu_arquivo = Menu(barra_menus, tearoff=0)
 arquivo = Arquivo()
-menu_arquivo.add_command(label='Novo arquivo', command=None)
+menu_arquivo.add_command(label='Novo arquivo', command=lambda: arquivo.novo_arquivo(text_space))
 menu_arquivo.add_command(label='Abrir arquivo', command=lambda: arquivo.abrir_arquivo(text_space))
 menu_arquivo.add_command(label='Salvar', command=lambda: arquivo.salvar_arquivo(text_space))
 menu_arquivo.add_command(label='Salvar como', command=lambda: arquivo.salvar_como(text_space))
 menu_arquivo.add_separator()
-menu_arquivo.add_command(label='Sair', command=None)
+menu_arquivo.add_command(label='Fechar', command=lambda: arquivo.fechar_bloco(text_space, root))
 barra_menus.add_cascade(label='Arquivo', menu=menu_arquivo)
 
 menu_editar = Menu(barra_menus, tearoff=0)
@@ -28,7 +28,5 @@ menu_editar.add_separator()
 menu_editar.add_command(label='Preferências', command=None)
 barra_menus.add_cascade(label='Editar', menu=menu_editar)
 root.config(menu=barra_menus)
-
-
 
 root.mainloop()
